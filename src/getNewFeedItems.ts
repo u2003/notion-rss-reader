54d9a119b3ac58e7d36de35c8be1919a3c99a67b
Wuuu0399 <2210914762@qq.com>
2025-02-11 17:48:43 +0800

    updated: only parse the new feed items created today

--- src/getNewFeedItems.ts
+++ src/getNewFeedItems.ts
@@ -1,10 +1,19 @@
 import Parser from 'rss-parser'
+import { timeDifference } from './helpers'
 
 const parser = new Parser()
 
 export const getNewFeedItems = async (feedUrl: string) => {
   const { items: newFeedItems } = await parser.parseURL(feedUrl)
 
-  // parse all the feed items that are created earlier than today for the first time
-  return newFeedItems
+  // updated: only parse the new feed items created today
+  return newFeedItems.filter((feedItem) => {
+    const { pubDate } = feedItem
+
+    if (!pubDate) return false
+
+    const publishedDate = new Date(pubDate).getTime() / 1000
+    const { diffInHours } = timeDifference(publishedDate)
+    return diffInHours === 0
+  })
 }
